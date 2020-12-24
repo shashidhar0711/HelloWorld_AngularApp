be01928db6d844278faab03a561fc4087b8978cf
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-12-24 21:04:51 +0530

    [sasic] Add . Created HelloWorld Angular App to Display Hello from Bridgelabz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'Hello';
   imgUrl;
@@ -12,16 +13,19 @@ export class AppComponent {
   userName: string= "";
   nameError: string= "";
 
+  // UC => 1 Display the Message Using One Way Data Binding
+  // UC => 2 Display the Bridgelabz Logo Image Using Property Binding 
   ngOnInit():void {
     this.title="Hello from Bridgelabz";
     this.imgUrl='../assets/BridgeLabz_Logo.png';
     this.url="https://www.bridgelabz.com/";
   }
-  
+  // UC => 3 Launch BridgeLabz Site in a New tab on Clicking Bridgelabz Logo Using Event Binding
   OnClick($event) {
     console.log("Mouse click",$event);
     window.open(this.url,"_blank");
   }
+  // UC => 5 Validates the User Name In case Error Display the error
   OnInput($event) {
     console.log("entering input",$event);
     var testRegex=RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+// Importing core modules of Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Whenever CLI is used to generate new component and services,
+// It will automatically update this file to import and add them here.
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
+// Using @NgModule decorator we provide additional metadata to specify the component,
+// the services, the imports..etc Further 
+// Decorators are used while processing, instantiation and at runtime.
 @NgModule({
   declarations: [
     AppComponent,
   ],
+// Various imports needed for Application can be added here.
   imports: [
     BrowserModule,
     AppRoutingModule,
